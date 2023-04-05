7d7b1053e295156f0a0991e7e00f9e2976d1725d
Daniel Barber <danbarber@learnzillion.com>
2023-04-05 17:15:16 -0500

    Use Neotree as file browser when opening directories

--- config/nvim/lua/plugins/neotree.lua
+++ config/nvim/lua/plugins/neotree.lua
@@ -5,5 +5,14 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
-  }
+  },
+
+  config = function ()
+    -- Unless you are still migrating, remove the deprecated commands from v1.x
+    vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+    require("neo-tree").setup({
+      hijack_netrw_behavior = "open_default"
+    })
+  end
 }

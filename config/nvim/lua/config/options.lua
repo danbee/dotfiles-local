ebb3f647c4dcacec0ef73715b16cf626809c5383
Daniel Barber <danbarber@learnzillion.com>
2023-04-05 17:15:06 -0500

    Enable line numbers

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -2,8 +2,11 @@ require('onedark').load()
 
 local indent = 2
 
+-- Tabstops
 vim.opt.expandtab = true
 vim.opt.shiftwidth = indent
 vim.opt.smartindent = true
 vim.opt.tabstop = indent
 
+-- Line numbers
+vim.opt.number = true

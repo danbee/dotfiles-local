494935644940633a07247344f5d23fdf79d43cd8
Dan Barber <github@danbarber.me>
2023-05-02 23:15:42 -0500

    Add dashboard header

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -16,10 +16,7 @@ return {
   {
     "glepnir/dashboard-nvim",
     event = "VimEnter",
-    config = function()
-      require("dashboard").setup({
-      })
-    end,
+    config = require("config.plugins.dashboard"),
     dependencies = { { "nvim-tree/nvim-web-devicons" } },
   },
   {

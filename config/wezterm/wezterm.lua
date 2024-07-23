1602a4a01cc0318e0d5a67ba29f28b1736c9d46f
Dan Barber <github@danbarber.me>
2024-07-23 16:26:30 -0500

    Updated Wezterm config

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,24 +1,24 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 local act = wezterm.action
 
-config.color_scheme = 'Afterglow'
-config.font = wezterm.font 'Source Code Pro'
-config.font_size = 14
+config.color_scheme = "Gruvbox Dark (Gogh)"
+config.font = wezterm.font "MonoLisa"
+config.font_size = 13
 
 config.use_fancy_tab_bar = false
 
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 
 config.keys = {
-  { key = "LeftArrow", mods = "CMD", action = act.ActivateTabRelative(-1) },
-  { key = "RightArrow", mods = "CMD", action = act.ActivateTabRelative(1) }
+	{ key = "LeftArrow", mods = "CMD", action = act.ActivateTabRelative(-1) },
+	{ key = "RightArrow", mods = "CMD", action = act.ActivateTabRelative(1) },
 }
 
 return config
